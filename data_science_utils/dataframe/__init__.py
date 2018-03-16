e7ed7085cd965a3f8e9a4d73907554b0a2bb5f66
Ahemad, Faizan <ahemf@amazon.com>
2018-03-16 21:03:19 +0530

    Bug fixes

--- data_science_utils/dataframe/__init__.py
+++ data_science_utils/dataframe/__init__.py
@@ -185,18 +185,22 @@ def find_correlated_pairs(df,thres):
                 print()
     return correlated_pairs
 
-def remove_correlated_pairs(df,thres,inplace=False):
+
+def remove_correlated_pairs(df, thres, inplace=False):
     _check_df(df)
     df_nulls = count_nulls(df).transpose()
-    correlated_pairs = find_correlated_pairs(df,thres)
-    from IPython.core.debugger import Tracer; Tracer()() 
+    correlated_pairs = find_correlated_pairs(df, thres)
+    from IPython.core.debugger import Tracer;
+    Tracer()()
     print(correlated_pairs)
     dropped_cols = list()
-    for (p1,p2) in correlated_pairs:
-        if(p1 not in dropped_cols and p2 not in dropped_cols):
+    for (p1, p2) in correlated_pairs:
+        print(p1)
+        print(p2)
+        if (p1 not in dropped_cols and p2 not in dropped_cols):
             p1_nulls = df_nulls[p1].values[0]
             p2_nulls = df_nulls[p2].values[0]
-            if(p1_nulls < p2_nulls):
+            if (p1_nulls < p2_nulls):
                 print(type(p2))
                 print(p2)
                 dropped_cols.append(p2)
@@ -204,13 +208,14 @@ def remove_correlated_pairs(df,thres,inplace=False):
                 print(type(p1))
                 print(p1)
                 dropped_cols.append(p1)
-    from IPython.core.debugger import Tracer; Tracer()() 
+    from IPython.core.debugger import Tracer;
+    Tracer()()
     print(len(dropped_cols))
     print(dropped_cols)
     dropped_cols = list(dropped_cols)
     # dropped_cols = list(np.sort(list(dropped_cols)))
-    new_df = drop_columns_safely(df,dropped_cols,inplace)
-    return (new_df,dropped_cols)
+    new_df = drop_columns_safely(df, dropped_cols, inplace)
+    return (new_df, dropped_cols)
 
 def detect_nan_columns(df):
     _check_df(df)
