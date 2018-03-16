b8bd902adaea506d40783ecfcfb0abe7459e4825
Ahemad, Faizan <ahemf@amazon.com>
2018-03-16 19:26:49 +0530

    Bug fixes

--- data_science_utils/dataframe/__init__.py
+++ data_science_utils/dataframe/__init__.py
@@ -198,8 +198,8 @@ def remove_correlated_pairs(df,thres,inplace=False):
                 dropped_cols.append(p2)
             else:
                 dropped_cols.append(p1)
-    print(dropped_cols)
     print(len(dropped_cols))
+    print(dropped_cols)
     dropped_cols = list(dropped_cols)
     # dropped_cols = list(np.sort(list(dropped_cols)))
     new_df = drop_columns_safely(df,dropped_cols,inplace)
