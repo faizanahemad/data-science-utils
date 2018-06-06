7da69f2717df24c94db14a06becc92d343e59ffe
Ahemad, Faizan <ahemf@amazon.com>
2018-06-06 14:20:36 +0530

    Timer Function

--- data_science_utils/misc/__init__.py
+++ data_science_utils/misc/__init__.py
@@ -1,6 +1,20 @@
 import inspect
-
+import time
 
 def print_function_code(func):
     print("".join(inspect.getsourcelines(func)[0]))
 
+
+def get_timer():
+    ctr=1
+    pv = -1e8
+    def timer():
+        nonlocal ctr
+        nonlocal pv
+        tc = time.time()%10000
+        diff = tc - pv
+        diff = 0 if diff>1e6 else diff
+        pv=tc
+        print("%s: %.3f, %.3f"%(ctr,tc,diff))
+        ctr=ctr+1
+    return timer
