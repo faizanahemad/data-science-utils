164d7c4eaf22238aac1458faf01ab44df6f5799b
Ahemad, Faizan <ahemf@amazon.com>
2018-07-22 12:07:34 +0530

    Adding Notebook for financial filtering

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(name='data_science_utils',
-      version='0.1.30',
+      version='0.1.31',
       description='Utils for use in python with pandas and numpy',
       url='https://github.com/faizanahemad/data-science-utils',
       author='Faizan Ahemad',
       author_email='fahemad3@gmail.com',
       license='MIT',
       install_requires=[
-          'numpy','pandas',
+          'numpy','pandas','beautifulsoup4','fastnumbers','more-itertools',
       ],
       keywords=['Pandas','numpy','data-science','IPython', 'Jupyter'],
       packages=find_packages(),
