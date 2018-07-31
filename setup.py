2b9304fbf7674a64d894d7e2345248a39aaa91f0
Ahemad, Faizan <ahemf@amazon.com>
2018-07-31 18:35:55 +0530

    added identity scaler

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(name='data_science_utils',
-      version='0.1.32',
+      version='0.1.33',
       description='Utils for use in python with pandas and numpy',
       url='https://github.com/faizanahemad/data-science-utils',
       author='Faizan Ahemad',
       author_email='fahemad3@gmail.com',
       license='MIT',
       install_requires=[
-          'numpy','pandas','beautifulsoup4','fastnumbers','more-itertools','dill',
+          'numpy','pandas','beautifulsoup4','fastnumbers','more-itertools','dill','stockstats',
       ],
       keywords=['Pandas','numpy','data-science','IPython', 'Jupyter'],
       packages=find_packages(),
