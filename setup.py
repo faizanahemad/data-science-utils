ab7c750eaea7acd7beff255609c036f741967bfe
Ahemad, Faizan <ahemf@amazon.com>
2018-08-11 13:43:45 +0530

    Updating tutorial notebook

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='data_science_utils',
       author_email='fahemad3@gmail.com',
       license='MIT',
       install_requires=[
-          'numpy','pandas','beautifulsoup4','fastnumbers','more-itertools','dill','stockstats',
+          'numpy','pandas','beautifulsoup4','fastnumbers','more-itertools','dill','stockstats','pytidylib',
       ],
       keywords=['Pandas','numpy','data-science','IPython', 'Jupyter'],
       packages=find_packages(),
