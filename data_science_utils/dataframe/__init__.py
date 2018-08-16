6114c5412edc64b0f16167fd688decccb43500fd
Ahemad, Faizan <ahemf@amazon.com>
2018-08-16 18:56:55 +0530

    count_nulls improve by showing percentage of nulls

--- data_science_utils/dataframe/__init__.py
+++ data_science_utils/dataframe/__init__.py
@@ -18,8 +18,9 @@ def get_column_names(df,sorted=True):
 def count_nulls(df):
     """Missing value count per column grouped by column name"""
     df_t = pd.DataFrame(df.isnull().sum()).rename(columns={0:"count"})
+    df_t["percent_null"] = df_t["count"]/df.shape[0]
     df_t.index.names = ["Column"]
-    return df_t.sort_values("count",ascending=False)
+    return df_t.sort_values("percent_null",ascending=False)
 
 def count_distinct_values(df):
     _check_df(df)
