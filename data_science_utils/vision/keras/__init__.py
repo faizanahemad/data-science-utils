28c4f4fd8c576cfdeb73f70843239f3de5aa5476
Faizan Ahemad <ahemf@amazon.com>
2019-06-02 20:14:07 +0530

    Bug Fix

--- data_science_utils/vision/keras/__init__.py
+++ data_science_utils/vision/keras/__init__.py
@@ -209,6 +209,25 @@ def evaluate(model, X_test, Y_test, classes,datagen=None, print_results=False, p
                                print_results=print_results, plot_results=plot_results)
 
 
+def plot_model_history(model_history):
+    fig, axs = plt.subplots(1,2,figsize=(18,6))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
 
 def show_examples(X,y,classes):
     rows = int(np.ceil(len(X)/5))
